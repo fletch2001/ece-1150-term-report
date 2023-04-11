70b27cb47c529224cdc5ad6ffda33a5d9329b2d3
fletch2001 <noahlichstein@gmail.com>
2023-04-10 21:18:00 -0400

    switching to non-random latencies. data is too varied. thread.py line 23 added random meshing to better replicate thread networking.

--- thread.py
+++ thread.py
@@ -20,16 +20,16 @@ def thread_setup(scale, lan_network):
     # mesh 'em
     for i in range(thread_num_nodes):
         for j in range(thread_num_nodes):
-            if i != j:
+            if i != j and random.randint(0, 1):
                 # connect all non-same nodes and add latency to edge
-                T.add_edge(f"thread_R{i}", f"thread_R{j}", latency=random.uniform(0.01, 0.1))
+                T.add_edge(f"thread_R{i}", f"thread_R{j}", latency=0.01)
 
     # add a random number of edge nodes to each mesh node
     for i in range(thread_num_nodes):
         for n in range(random.randint(1, 10)):
             T.add_node(f"thread_dev_{i}_{n}")
             # add latency again
-            T.add_edge(f"thread_R{i}", f"thread_dev_{i}_{n}", latency=random.uniform(0.01, 0.1))
+            T.add_edge(f"thread_R{i}", f"thread_dev_{i}_{n}", latency=0.01)
 
     # randomly choose leader
     leader_index = random.randint(0, thread_num_nodes - 1)
@@ -48,10 +48,10 @@ def thread_setup(scale, lan_network):
 
         border_indexes.append(i)
 
+    total_nodes = len(T.nodes)
+
     # compose/join networks into one lan network
     lan_network = nx.compose(T, lan_network)
-
-    plot_graph(lan_network)
 
     # set border routers
     for n in range(num_borders):
@@ -60,14 +60,7 @@ def thread_setup(scale, lan_network):
         # connect border routers to LAN
         lan_network.add_edge("lan_router", f"thread_R{border_indexes[n]}", latency=random.uniform(0.01, 0.1))
 
-    return lan_network
-
-def route(network, source, dest):
-    # if dest not in network.nodes:
-        # add routing delay?
-
-    path = nx.shortest_path(network, source, dest, "latency")
-    path_latency = sum([network[path[i]][path[i+1]]["latency"] for i in range(len(path)-1)])
+    return lan_network, total_nodes
 
 # time
 # hops
